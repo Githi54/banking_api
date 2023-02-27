837541b17e65121d24e8ac5ab09e051b7080d654
Mikita Havryliuk <chinter.games1@gmail.com>
2023-02-27 13:54:45 +0200

    some fixes

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,5 +1,5 @@
 import { Module } from '@nestjs/common';
-import { ConfigModule } from '@nestjs/config';
+// import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { BankModule } from './bank/bank.module';
 import { Bank } from '../database/entities/bank.entity';
@@ -8,17 +8,17 @@ import { TransactionsModule } from './transactions/transactions.module';
 
 @Module({
   imports: [
-    ConfigModule.forRoot({
-      envFilePath: `.${process.env.NODE_ENV}.env`,
-      isGlobal: true,
-    }),
+    // ConfigModule.forRoot({
+    //   envFilePath: `.${process.env.NODE_ENV}.env`,
+    //   isGlobal: true,
+    // }),
     TypeOrmModule.forRoot({
       type: 'postgres',
-      host: process.env.POSTGRES_HOST,
-      port: Number(process.env.POSTGRES_PORT),
-      username: process.env.POSTGRES_USER,
-      password: `${process.env.POSTGRES_PASSWORD}`,
-      database: process.env.POSRGRES_DB,
+      host: 'localhost',
+      port: 5432,
+      username: 'postgres',
+      password: 'BAscjbaiu23e4',
+      database: 'banking-api',
       entities: [Bank],
       autoLoadEntities: true,
       synchronize: true,
