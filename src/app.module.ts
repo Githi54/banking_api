735ee357fcbfdbb6cfcbf658372010c0ce6c2fa2
Mikita Havryliuk <chinter.games1@gmail.com>
2023-02-28 13:50:35 +0200

    for Docker

--- src/app.module.ts
+++ src/app.module.ts
@@ -14,7 +14,7 @@ import { TransactionsModule } from './transactions/transactions.module';
     }),
     TypeOrmModule.forRoot({
       type: 'postgres',
-      host: 'localhost',
+      host: 'postgres',
       port: 5432,
       username: 'postgres',
       password: 'BAscjbaiu23e4',
